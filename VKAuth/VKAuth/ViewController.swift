a5c764a326709e364a4724246b0e3205a9a7c2f0
Nikita Entin <entin@vbankcenter.ru>
2021-08-04 16:41:05 +0300

    initial commit

---
+++ VKAuth/VKAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKAuth
+//
+//  Created by Nikita Entin on 04.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
